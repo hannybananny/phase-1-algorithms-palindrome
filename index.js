64e943cea27eb0aa0d61daab2a237e770a11633e
Hannah <hannahmgrout@gmail.com>
2022-04-29 19:54:14 -0700

    commit tree 7b5b07a78b37a72c836bded9d690f58433952549

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let reverseWord = word.split('').reverse().join('')
+  if(word === reverseWord){
+    return true
+  }
+  else{
+    return false
+  }
 }
 
 /* 
-  Add your pseudocode here
+  Argument is recieved as string
+  Check to see if string is palindrome
+    Split string
+    Reverse string
+    Join string
+  If string is palindrome, return true
+  If string is not palindrome, return false
 */
 
 /*
-  Add written explanation of your solution here
+  To check to see if the word is a palindrome,
+  the reveresed word should equal the initial word.
+  The split() method breaks the string into individual letters
+  The reverse() method will reverse the order of the letters
+  The join() method will join the letters into a string
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("hannah"))
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome('carrot'))
+  
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
